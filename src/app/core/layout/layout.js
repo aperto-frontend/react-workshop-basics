de3ff7820e3133be68e6a55deede17f683bfcc72
Sebastian Fitzner <sebastian.fitzner@aperto.com>
2017-12-19 00:55:41 +0100

    Add scss support and general style

--- src/app/core/layout/layout.js
+++ src/app/core/layout/layout.js
@@ -1,11 +1,18 @@
-import React from 'react';
+import React, {Fragment} from 'react';
+import './layout.scss';
 
 export default function Layout({children}) {
 	return (
-		<div className="page-wrapper">
-			<header><h1>My custom header</h1></header>
-			<main>{children}</main>
-			<footer><h2>Footer</h2></footer>
-		</div>
+		<Fragment>
+			<header className="r-header is-container">
+				<h1 className="header__headline">My custom header</h1>
+			</header>
+			<main className="r-main is-container ">
+				{children}
+			</main>
+			<footer className="r-footer is-container ">
+				<h2>Footer</h2>
+			</footer>
+		</Fragment>
 	)
 }
